8d81ac52c37c9a3b029fc4c9b47cfc74d2e0f266
0918901 <0918901@hr.nl>
2017-03-28 15:02:34 +0200

    [UPDATED] Repository - Files Structure

--- src/ConnectMySQL/DBObjects.java
+++
@@ -1,9 +0,0 @@
-package ConnectMySQL;
-
-public class DBObjects {
-    private final static ConnectMySQL schoolConnection = new ConnectMySQL("jdbc:mysql://localhost:3306/KrewBanking", "root", "root");
-
-    public static ConnectMySQL getSchoolConnection(){
-        return DBObjects.schoolConnection;
-    }
-}
